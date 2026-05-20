ff4817fd4b6c922f1e5f32f37262aac4f848e032
Eddy Mboya <eddyguya128@gmail.com>
2026-05-20 09:24:26 +0300

    First website upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("JavaScript connected");
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
